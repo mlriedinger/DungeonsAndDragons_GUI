638c5321f05a1537d78fdb865609a3024f34c495
mlriedinger <marie-laure.riedinger@le-campus-numerique.fr>
2020-06-08 10:39:47 +0200

    Etape 2 : remplissage classe GUI pour afficher une fenêtre vide

--- src/Warriors/Main.java
+++ src/Warriors/Main.java
@@ -1,10 +1,13 @@
 package Warriors;
 
+import Warriors.View.GUI;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		GUI gui = new GUI();
 	}
 
 }
--- src/Warriors/View/GUI.java
+++ src/Warriors/View/GUI.java
@@ -1,5 +1,16 @@
 package Warriors.View;
 
-public class GUI {
+import javax.swing.JFrame;
+
+public class GUI extends JFrame {
+	
+	public GUI() {
+		this.setSize(1280, 720);
+		this.setTitle("D&D Warriors Campus Num Chambéry");
+		this.setLocation(10, 10);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setVisible(true);
+	}
 
 }
